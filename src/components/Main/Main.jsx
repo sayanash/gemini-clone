df8b6d6e50a9d4bb2a71407533765564976b47b4
Sayan Ash <sayanash347200@gmail.com>
2025-04-06 16:11:54 +0530

    Completed UI parts

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -31,6 +31,19 @@ const Main = () => {
                     <img src={assets.code_icon} alt="" />
                 </div>
             </div>
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input type="text" placeholder='Enter a prompt here'/>
+                    <div>
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img src={assets.send_icon} alt="" />
+                    </div>
+                </div>
+                <p className="bottom-info">
+                  Gemini may display inaccurate information about people, places, or facts. Always verify with reliable sources. Use at your own risk. 
+                </p>
+            </div>
         </div>
     </div>
   )
